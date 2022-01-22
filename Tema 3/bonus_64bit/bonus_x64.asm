6972be25f1495e447e4b6483dc8819aedb04ac45
Laurentiu Ursu <laurentiu.ursu@gmail.com>
2022-01-22 10:57:51 -0500

    Commited source code for task 4 and bonuses

--- Tema 3/bonus_64bit/bonus_x64.asm
+++ Tema 3/bonus_64bit/bonus_x64.asm
@@ -8,6 +8,73 @@ section .text
 ;  The resulting array is stored in v
 intertwine:
 	enter 0, 0
-
+	sub 	rsp, 44
+	mov 	DWORD [rbp - 4], 0    ; i
+	mov 	DWORD [rbp - 8], 0    ; j
+	mov 	DWORD [rbp - 12], 0   ; k
+	mov   DWORD [rbp - 16], esi ; n1
+	mov		DWORD [rbp - 20], ecx ; n2
+	mov   [rbp - 28], rdi 		  ; v1
+	mov 	[rbp - 36], rdx 		  ; v2
+	mov 	[rbp - 44], r8 			  ; v
+both_while:
+	mov   eax, DWORD [rbp - 12]    ; put k in rax
+	mov 	rcx, QWORD 2
+	cdq
+	idiv	rcx
+	cmp 	rdx, 1  					; k % 2
+	jne		add_first_vect
+	mov 	rax, [rbp - 36]    ; v2
+	mov  	ebx, DWORD [rbp - 8]     ; j
+	mov	  ecx, DWORD [rax + 4 * rbx] ; v2[j]
+	mov 	rax, [rbp - 44]     ; v
+	mov	  ebx, [rbp - 12]    ; k
+	mov 	DWORD [rax + 4 * rbx], ecx  ; v[k] = v2[j]
+	inc		DWORD [rbp - 12]   ; k++
+	inc 	DWORD [rbp - 8]  	 ; j++
+	jmp		end_if
+add_first_vect:
+	mov 	rax, [rbp - 28]     ; v1
+	mov 	ebx, DWORD [rbp - 4]     ; j
+	mov		ecx, DWORD [rax + 4 * rbx]
+	mov 	rax, [rbp - 44]     ; v
+	mov 	ebx, DWORD [rbp - 12]    ; k
+	mov 	DWORD [rax + 4 * rbx], ecx  ; v[k] = v1[i]
+	inc		DWORD [rbp - 12] 	 ; k++
+	inc 	DWORD [rbp - 4]		 ; i++
+end_if:
+	mov   eax, DWORD [rbp - 4]
+	cmp		eax, [rbp - 16]
+	je		both_while_end
+	mov 	eax, DWORD [rbp - 8]
+	cmp 	eax, [rbp - 20]
+	jb		both_while
+both_while_end:
+	mov 	eax, DWORD [rbp - 4] ; i
+	cmp 	eax, [rbp - 16]
+	je		j_while_end
+	mov 	rax, [rbp - 28]     ; v1
+	mov   ebx, DWORD [rbp - 4]  ; i
+	mov		ecx, DWORD [rax + 4 * rbx]
+	mov 	rax, [rbp - 44]     ; v
+	mov 	ebx, DWORD [rbp - 12]    ; k
+	mov 	DWORD [rax + 4 * rbx], ecx  ; v[k] = v1[i]
+	inc		DWORD [rbp - 12] 	 ; k++
+	inc 	DWORD [rbp - 4]		 ; i++
+	jmp 	both_while_end
+j_while_end:
+	mov 	eax, [rbp - 8] ; j
+	cmp 	eax, [rbp - 20]
+	je  	func_end
+	mov 	rax, [rbp - 36]    ; v2
+	mov 	ebx, DWORD [rbp - 8]     ; j
+	mov		rcx, [rax + 4 * rbx]
+	mov 	rax, [rbp - 44]    ; v
+	mov 	ebx, DWORD [rbp - 12]    ; k
+	mov 	DWORD [rax + 4 * rbx], ecx  ; v[k] = v2[j]
+	inc		DWORD [rbp - 12]   ; k++
+	inc 	DWORD [rbp - 8]  	 ; j++
+	jmp 	j_while_end
+func_end:
 	leave
 	ret
--- Tema 3/bonus_cpuid/bonus_cpuid.asm
+++ Tema 3/bonus_cpuid/bonus_cpuid.asm
@@ -7,8 +7,15 @@ section .text
 ;
 ;  reads the manufacturer id string from cpuid and stores it in id_string
 cpu_manufact_id:
-	enter 	0, 0
-	
+	enter	0, 0
+	pusha
+	xor 	eax, eax
+	cpuid											; run cpuid with eax 0 to get ID
+	mov		eax, [ebp + 8]
+	mov 	DWORD [eax], ebx
+	mov 	DWORD [eax + 4], edx
+	mov 	DWORD [eax + 8], ecx
+	popa
 	leave
 	ret
 
@@ -18,8 +25,41 @@ cpu_manufact_id:
 ;  if a feature is supported, 1 is written in the corresponding variable
 ;  0 is written otherwise
 features:
-	enter 	0, 0
-	
+	enter	0, 0
+	pusha
+	mov		eax, 1
+	cpuid
+	mov 	eax, 0x20        ; turn on 5th bit to check for vmx
+	and 	eax, ecx
+	cmp 	eax, 0
+	mov 	eax, [ebp + 8]
+	je 		no_vmx           ; if present add it to char
+	mov 	DWORD [eax], 1
+	jmp 	rdrand
+no_vmx:
+	mov 	DWORD [eax], 0
+rdrand:
+	mov 	eax, 0x40000000  ; turn on 30th bit to check for rdrand
+	and 	eax, ecx
+	cmp 	eax, 0
+	mov 	eax, [ebp + 12]
+	je 		no_rdrand
+	mov 	DWORD [eax], 1
+	jmp 	avx
+no_rdrand:
+	mov 	DWORD [eax], 0
+avx:
+	mov 	eax, 0x10000000; turn on 28th bit to check for avx 	
+	and 	eax, ecx
+	cmp 	eax, 0
+	mov 	eax, [ebp + 16]
+	je 		no_avx
+	mov 	DWORD [eax], 1
+	jmp 	func_end
+no_avx:
+	mov 	DWORD [eax], 0
+func_end:
+	popa
 	leave
 	ret
 
@@ -28,7 +68,19 @@ features:
 ;  reads from cpuid the cache line size, and total cache size for the current
 ;  cpu, and stores them in the corresponding parameters
 l2_cache_info:
-	enter 	0, 0
-	
+	enter 0, 0
+	pusha
+	mov 	eax, 0x80000006   ; mov to eax function to get cpuid info
+	cpuid	
+	mov 	eax, 0xff         ; get the least significant byte of ecx -> LEVEL2_CACHE_LINESIZE
+	and 	eax, ecx
+	mov 	ebx, [ebp + 8]
+	mov 	DWORD [ebx], eax
+	mov 	eax, 0xffff0000   ; get first two bytes most significant of ecx -> LEVEL2_CACHE_SIZE
+	and 	eax, ecx
+	shr 	eax, 16 					; shift right to get good value
+	mov 	ebx, [ebp + 12]
+	mov 	DWORD [ebx], eax
+	popa
 	leave
 	ret
